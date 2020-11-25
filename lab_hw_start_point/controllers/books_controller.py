aa3fec7b0a8250251119485790dbde132acc6369
Kurt S Steffan <kurt.s.steffan@gmail.com>
2020-11-25 17:51:17 +0000

    Added delete a book functionality

--- lab_hw_start_point/controllers/books_controller.py
+++ lab_hw_start_point/controllers/books_controller.py
@@ -55,4 +55,7 @@ def create_book():
 
 # DELETE
 # DELETE '/books/<id>'
-
+@books_blueprint.route("/books/<id>/delete", methods=["POST"])
+def delete_book(id):
+    book_repository.delete(id)
+    return redirect('/books')
